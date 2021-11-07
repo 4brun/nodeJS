3836dd173f8b2798765b9acd547daddfd660be10
Dmitriy <dabrun@gmail.com>
2021-11-07 16:22:07 +0300

    add web interface

--- index.js
+++ index.js
@@ -1,50 +1,51 @@
 const fs = require('fs')
 const path = require('path');
-const inquirer = require('inquirer');
-let directory = process.cwd()
+const http = require('http')
+
+let content = ``;
 
 const isDirectory = dirname => {
    const stat = fs.lstatSync(dirname)
    return stat.isDirectory()
 }
 
-const fileList = dirname => fs.readdirSync(dirname)
-
-const searchString = date => {
-   inquirer.prompt([{
-      name: 'searchString',
-      type: 'input',
-      message: 'Введите строку для поиска в файле: '
-   }])
-      .then(answer => {
-         const index = date.indexOf(answer.searchString) // ищем строку в файле
-         if (index !== -1) {
-            console.log('Найдено: \n' + date.slice((index - 10), (index + 25)));
-         } else {
-            console.log("Ничего не найдено");
-         }
-      })
-      .catch(error => console.log(error))
-}
-
-const getList = directory => inquirer.prompt([
-   {
-      name: 'filename',
-      type: 'list',
-      message: 'Выберете файл или папку для чтения: ',
-      choices: fileList(directory)
+const server = http.createServer((req, res) => {
+   if (req.url === "/favicon.ico") { // долго мучался, почему то падало всё из за этого favicon.ico
+      res.writeHead(200, {
+         "Content-Type": "image/x-icon",
+         "Cache-Control": "max-age=31536000",
+      });
+      res.end();
+      return;
    }
-])
-   .then(answer => {
-      directory = path.resolve(directory, answer.filename) // перезаписываем путь для текущей дирректории
+   res.writeHead(200, {
+      "Content-Type": "text/html"
+   });
 
-      if (isDirectory(directory)) {
-         getList(directory) // запускаем рекурсию с новой дирректории
-      } else {
-         const date = fs.readFileSync(directory, 'utf-8')
-         console.log(date) // выводим содержимое в консоль
-         searchString(date) // запускаем поиск строки
+   let somePath = req.url !== "" ? req.url : "";
+   const fullPath = path.resolve(process.cwd(), somePath);
+
+   if (!isDirectory(fullPath)) {
+      content = fs.readFileSync(fullPath, "utf-8");
+   } else {
+      const list = fs.readdirSync(fullPath);
+      let prep = req.url;
+      let up = "";
+
+      if (prep.length > 1) {
+         prep += "/";
+         const backUrl = "/" + prep.split("/").slice(1, -2).join("/");
+         up = `<li><a href=${backUrl}>..</a></li>`;
       }
-   })
 
-getList(directory)
+      content = `<ul>${up}`;
+      for (let i = 0; i < list.length; i++) {
+         content += `<li><a href=${prep}${list[i]}>${list[i]}</a></li>`
+      }
+      content += `</ul>`
+   }
+
+   res.end(content);
+})
+
+server.listen(3001);
